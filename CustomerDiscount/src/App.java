9499e760a2b1676ff7cac0ccdaffc6c2e320f320
Ali Hussain (Coolsonickirby) <alihussain2001@gmail.com>
2021-10-20 13:40:32 -0400

    Do changes prof said

--- CustomerDiscount/src/App.java
+++ CustomerDiscount/src/App.java
@@ -1,4 +1,33 @@
+/*|----------------------------------------------------------------|*
+ *| CIS-171 Java Programming                                       |*
+ *| Assignment #X - Assignment Title                               |*
+ *| Written By: Ali Hussain (Coolsonickirby/Random)                |*
+ *|                                                                |*
+ *| Notes: Blah Blah Blah                                          |*
+ *|----------------------------------------------------------------|*
+ */
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class App {
+    public static Scanner reader = new Scanner(System.in); // Create globally so main and prompt can reference it
+    public static String prompt(String prompt) { // Emulate Python prompt function
+        System.out.print(prompt);
+        return reader.nextLine();
+    }
+    public static String choice(String prompt, String patternString){
+        Pattern p = Pattern.compile(patternString);
+        String choice = prompt(prompt);
+
+        while(!p.matcher(choice).matches()){
+            System.out.println("Invalid choice!");
+            choice = prompt(prompt);
+        }
+
+        return choice;
+    }
+
     public static void main(String[] args) throws Exception {
         PreferredCustomer customer = new PreferredCustomer(
             "name",
@@ -8,9 +37,18 @@ public class App {
             0.00
         );
 
-        for(int i = 0; i <= 1500; i += 500){
-            customer.add(i);
-            System.out.format("Total Purchase Amount: $%s - Discount Amount: %s\n", customer.getTotalAmount(), customer.getDiscountAmount());
+        double purchase;
+        double discount;
+        while(true){
+            purchase = Double.parseDouble(choice("Enter purchase amount: ", "^\\d+([.][0-9]+)?$"));
+            discount = customer.getDiscountAmount();
+            customer.add(purchase - (purchase * discount));
+            System.out.format("Account Number: %s\nPurchase Amount: $%s\nDiscount: $%s\nTotal Purchase Amount: $%s\n---------------------\n",
+                customer.getCustomerNumber(),
+                purchase,
+                discount * purchase,
+                customer.getTotalAmount()
+            );
         }
     }
 }
--- CustomerDiscount/src/PreferredCustomer.java
+++ CustomerDiscount/src/PreferredCustomer.java
@@ -1,11 +1,9 @@
 public class PreferredCustomer extends Customer {
-    private double discountAmount;
     private double totalAmount;
 
     PreferredCustomer(String name, String address, String phoneNumber, boolean isOnMailingList, double totalAmount) {
         super(name, address, phoneNumber, isOnMailingList);
         this.totalAmount = totalAmount;
-        calcDiscountAmount();
     }
 
     public double getTotalAmount() {
@@ -14,26 +12,22 @@ public class PreferredCustomer extends Customer {
 
     public void setTotalAmount(double totalAmount) {
         this.totalAmount = totalAmount;
-        this.calcDiscountAmount();
     }
 
     public void add(double amount) {
         this.totalAmount += amount;
-        this.calcDiscountAmount();
     }
 
-    private void calcDiscountAmount() {
+    public double getDiscountAmount() {
         if (PreferredCustomer.CheckBetweenRange(this.totalAmount, 500, 999.99))
-            this.discountAmount = 0.10;
+            return 0.10;
         else if (PreferredCustomer.CheckBetweenRange(this.totalAmount, 1000, 1999.99))
-            this.discountAmount = 0.15;
+            return 0.15;
         else if (PreferredCustomer.CheckBetweenRange(this.totalAmount, 2000, Double.MAX_VALUE))
-            this.discountAmount = 0.25;
+            return 0.25;
         else
-            this.discountAmount = 0.00;
+            return 0.00;
     }
-
-    public double getDiscountAmount() { return this.discountAmount; }
 
     private static boolean CheckBetweenRange(double value, double min, double max) {
         return value <= max && value >= min;

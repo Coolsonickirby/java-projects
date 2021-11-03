b460e818762d14495d9667e8319533d787bbb163
Ali Hussain <alihussain2001@gmail.com>
2021-11-03 14:27:08 -0400

    Update changes done at College

--- JavaFXIntro/src/App.java
+++ JavaFXIntro/src/App.java
@@ -5,8 +5,10 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.HPos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
@@ -18,11 +20,15 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 import javafx.stage.WindowEvent;
+import javafx.scene.paint.Color;
  
 public class App extends Application {
     public static boolean VIEW_EMPLOYEES = false;
+    public static final String BTN_STYLE = "-fx-padding: 10px;";
     public static ObservableList<Employee> EMPLOYEES = FXCollections.observableArrayList();
     public static void main(String[] args) {
         launch(args);
@@ -32,14 +38,38 @@ public class App extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Employee Manager");
         primaryStage.setResizable(false);
-
+   
         StackPane root = new StackPane();
+        Scene main = new Scene(root, 512, 256);
+        primaryStage.setScene(main);
+
+        
         GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
         StackPane.setMargin(grid, new Insets(15, 50, 50, 50));
         root.getChildren().add(grid);
 
-        Scene main = new Scene(root, 512, 256);
-        primaryStage.setScene(main);
+        HBox header = new HBox();
+        Label title = new Label("Employee Manager");
+        title.setFont(new Font(30.0));
+        StackPane iconContainer = new StackPane();
+        Circle outline = new Circle();
+        outline.setCenterX(10.0f);
+        outline.setCenterY(10.0f);
+        outline.setRadius(10.0f);
+        outline.setFill(Color.TRANSPARENT);
+        outline.setStroke(Color.BLACK);
+        
+        
+        iconContainer.getChildren().addAll(outline);
+        
+        header.getChildren().addAll(title, iconContainer);
+        
+        GridPane.setHalignment(header, HPos.CENTER);
+        GridPane.setColumnSpan(header, GridPane.REMAINING);        
+        GridPane.setConstraints(header, 0, 0);
+        grid.getChildren().add(header);
 
         Label lblName = new Label("Employee Name:");
         TextField txtName = new TextField();
@@ -48,12 +78,12 @@ public class App extends Application {
         Label lblDOB = new Label("Date of Birth:");
         DatePicker dpDOB = new DatePicker();
         dpDOB.setValue(LocalDate.now());
-
+        
         Label lblPhoneNumber = new Label("Phone Number:");
         TextField txtPhoneNumber = new TextField();
         txtPhoneNumber.setText("");
 
-        Label lblRole = new Label("Phone Number:");
+        Label lblRole = new Label("Role:");
         ChoiceBox<String> choRole = new ChoiceBox<String>(FXCollections.observableArrayList(
             "Manager", "Chef", "Enginner", "System Admin"
         ));
@@ -84,20 +114,29 @@ public class App extends Application {
         });
 
 
-        GridPane.setConstraints(lblName, 0, 0);
-        GridPane.setConstraints(txtName, 1, 0);
+        GridPane.setConstraints(lblName, 0, 1);
+        GridPane.setConstraints(txtName, 1, 1);
 
-        GridPane.setConstraints(lblDOB, 0, 1);
-        GridPane.setConstraints(dpDOB, 1, 1);
+        GridPane.setConstraints(lblDOB, 0, 2);
+        GridPane.setConstraints(dpDOB, 1, 2);
 
-        GridPane.setConstraints(lblPhoneNumber, 0, 2);
-        GridPane.setConstraints(txtPhoneNumber, 1, 2);
+        GridPane.setConstraints(lblPhoneNumber, 0, 3);
+        GridPane.setConstraints(txtPhoneNumber, 1, 3);
         
-        GridPane.setConstraints(lblRole, 0, 3);
-        GridPane.setConstraints(choRole, 1, 3);
+        GridPane.setConstraints(lblRole, 0, 4);
+        GridPane.setConstraints(choRole, 1, 4);
         
-        GridPane.setConstraints(viewEmp, 0, 4);
-        GridPane.setConstraints(addEmp, 1, 4);
+        GridPane.setConstraints(viewEmp, 0, 5);
+        GridPane.setConstraints(addEmp, 1, 5);
+
+        lblName.setMinWidth(130);
+        dpDOB.setMinWidth(340);
+        choRole.setMinWidth(340);
+        
+        GridPane.setHalignment(addEmp, HPos.RIGHT);
+
+        viewEmp.setStyle(BTN_STYLE);
+        addEmp.setStyle(BTN_STYLE);
 
         grid.getChildren().addAll(lblName, txtName, lblDOB, dpDOB, lblPhoneNumber, txtPhoneNumber, lblRole, choRole, viewEmp, addEmp);
 
@@ -125,7 +164,7 @@ public class App extends Application {
         
         TableColumn<Employee, Integer> idHeader = new TableColumn<Employee, Integer>("ID");
         idHeader.setCellValueFactory(new PropertyValueFactory<Employee, Integer>("id"));
-        
+       
         TableColumn<Employee, String> nameHeader = new TableColumn<Employee, String>("Name");
         nameHeader.setCellValueFactory(new PropertyValueFactory<Employee, String>("name"));
         

6880ef8b0f0fe3c694831f8d6da6e39f7f28d048
Ali Hussain (Coolsonickirby) <alihussain2001@gmail.com>
2021-11-08 09:17:52 -0500

    Add message and prompt box

--- Pizza/src/App.java
+++ Pizza/src/App.java
@@ -7,13 +7,17 @@ import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
@@ -24,179 +28,202 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Optional;
 import java.util.stream.Collectors;
- 
+
 public class App extends Application {
     public static final ArrayList<Size> PIZZA_SIZES = new ArrayList<Size>();
     public static final ArrayList<Topping> PIZZA_TOPPINGS = new ArrayList<Topping>();
     public static final ObservableList<Pizza> PIZZAS = FXCollections.observableArrayList();
     public static final NumberFormat FORMATTER = new DecimalFormat("#0.00");
     public static TableView<Pizza> TABLE_VIEW = new TableView<Pizza>();
+
     public static void main(String[] args) {
         setup();
         launch(args);
     }
 
-    public static void setup(){
-        //#region Setup Pizza Sizes
+    public static void setup() {
+        // #region Setup Pizza Sizes
         PIZZA_SIZES.add(new Size("Small", 7.00));
         PIZZA_SIZES.add(new Size("Medium", 9.00));
         PIZZA_SIZES.add(new Size("Large", 15.00));
         PIZZA_SIZES.add(new Size("Extra Large", 19.00));
-        //#endregion
+        // #endregion
 
-        //#region Setup Toppings
+        // #region Setup Toppings
         PIZZA_TOPPINGS.add(new Topping("Cheese", 0.00));
         PIZZA_TOPPINGS.add(new Topping("Olives", 1.00));
         PIZZA_TOPPINGS.add(new Topping("Meat", 1.00));
         PIZZA_TOPPINGS.add(new Topping("Pineapple", 1.00));
         PIZZA_TOPPINGS.add(new Topping("Peppers", 1.00));
         PIZZA_TOPPINGS.add(new Topping("Mushrooms", 1.00));
-        //#endregion
+        // #endregion
     }
-    
+
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Pizza Menu");
+        primaryStage.setResizable(false);
+
+        Label lblCashier = new Label("Cashier Name:");
+        TextField txtCashier = new TextField();
+
         Button btnAdd = new Button("Add Pizza");
         Button btnEdit = new Button("Edit Pizza");
         Button btnDel = new Button("Remove Pizza");
 
-        
         TABLE_VIEW.setEditable(false);
 
         TableColumn<Pizza, String> pizzaID = new TableColumn<Pizza, String>("Pizza ID");
-        pizzaID.setCellValueFactory(
-            new PropertyValueFactory<Pizza, String>("idString")
-        );
+        pizzaID.setCellValueFactory(new PropertyValueFactory<Pizza, String>("idString"));
 
         TableColumn<Pizza, String> pizzaSize = new TableColumn<Pizza, String>("Pizza Size");
-        pizzaSize.setCellValueFactory(
-            new PropertyValueFactory<Pizza, String>("sizeString")
-        );
+        pizzaSize.setCellValueFactory(new PropertyValueFactory<Pizza, String>("sizeString"));
 
         TableColumn<Pizza, String> pizzaToppings = new TableColumn<Pizza, String>("Pizza Toppings");
-        pizzaToppings.setCellValueFactory(
-            new PropertyValueFactory<Pizza, String>("toppingsString")
-        );
+        pizzaToppings.setCellValueFactory(new PropertyValueFactory<Pizza, String>("toppingsString"));
 
         TableColumn<Pizza, String> pizzaExtras = new TableColumn<Pizza, String>("Extra Instructions");
-        pizzaExtras.setCellValueFactory(
-            new PropertyValueFactory<Pizza, String>("extras")
-        );
+        pizzaExtras.setCellValueFactory(new PropertyValueFactory<Pizza, String>("extras"));
 
         TableColumn<Pizza, String> pizzaTotalPrice = new TableColumn<Pizza, String>("Pizza Total Price");
-        pizzaTotalPrice.setCellValueFactory(
-            new PropertyValueFactory<Pizza, String>("totalPriceString")
-        );
+        pizzaTotalPrice.setCellValueFactory(new PropertyValueFactory<Pizza, String>("totalPriceString"));
 
         TABLE_VIEW.setItems(PIZZAS);
         TABLE_VIEW.getColumns().addAll(pizzaID, pizzaSize, pizzaToppings, pizzaExtras, pizzaTotalPrice);
+        TABLE_VIEW.getColumns().stream().forEach(column -> {
+            column.setMinWidth(120);
+            column.setResizable(false);
+        });
 
         btnAdd.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event){               
+            public void handle(ActionEvent event) {
                 showPizzaMenu(0);
             }
         });
-        
+
         btnEdit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event){               
-                if(TABLE_VIEW.getSelectionModel().selectedItemProperty().get() != null){ showPizzaMenu(TABLE_VIEW.getSelectionModel().selectedItemProperty().get().getId()); }
+            public void handle(ActionEvent event) {
+                if (TABLE_VIEW.getSelectionModel().selectedItemProperty().get() != null) {
+                    showPizzaMenu(TABLE_VIEW.getSelectionModel().selectedItemProperty().get().getId());
+                }else {
+                    ShowMessage("Please select an item in the table first!");
+                }
             }
         });
 
         btnDel.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event){               
-                if(TABLE_VIEW.getSelectionModel().selectedItemProperty().get() != null){ PIZZAS.remove(TABLE_VIEW.getSelectionModel().selectedIndexProperty().get()); }
+            public void handle(ActionEvent event) {
+                if (TABLE_VIEW.getSelectionModel().selectedItemProperty().get() != null) {
+                    System.out.println(ShowPrompt("Hi"));
+                    PIZZAS.remove(TABLE_VIEW.getSelectionModel().selectedIndexProperty().get());
+                }else{
+                    ShowMessage("Please select an item in the table first!");
+                }
             }
         });
 
         HBox btns = new HBox(btnAdd, btnEdit, btnDel);
         btns.setSpacing(10);
-    
+
         VBox container = new VBox();
         container.setPadding(new Insets(10, 10, 10, 10));
         container.setSpacing(10);
-        container.getChildren().addAll(btns, TABLE_VIEW);
+        container.getChildren().addAll(lblCashier, txtCashier, btns, TABLE_VIEW);
 
-        primaryStage.setScene(new Scene(container, 512, 512));
+        primaryStage.setScene(new Scene(container, 630, 512));
         primaryStage.show();
     }
 
-    public static void showPizzaMenu(int id){
+    public static void showPizzaMenu(int id) {
         Pizza currentPizza = PIZZAS.stream().filter(entry -> entry.getId() == id).findFirst().orElse(null);
 
         Stage primaryStage = new Stage();
         primaryStage.setTitle("Pizza");
 
         Label lblSizes = new Label("Select a pizza size:");
-        ListView<String> lvSizes = new ListView<String>(FXCollections.observableArrayList(
-            PIZZA_SIZES.stream().map(entry -> entry.toString()).collect(Collectors.toList())
-        ));
+        ListView<String> lvSizes = new ListView<String>(FXCollections
+                .observableArrayList(PIZZA_SIZES.stream().map(entry -> entry.toString()).collect(Collectors.toList())));
 
-        if(currentPizza != null){ lvSizes.getSelectionModel().select(currentPizza.getPizzaSize().toString()); }
+        if (currentPizza != null) {
+            lvSizes.getSelectionModel().select(currentPizza.getPizzaSize().toString());
+        }
 
         Label lblToppings = new Label("Select the toppings you'd like:");
-        
+
         FlowPane toppings = new FlowPane();
         toppings.setHgap(10);
         toppings.setVgap(10);
         toppings.setOrientation(Orientation.HORIZONTAL);
 
         ArrayList<String> selectedToppings = currentPizza != null ? new ArrayList<String>() : null;
-        if(currentPizza != null){ currentPizza.getToppings().stream().forEach(entry -> selectedToppings.add(entry.toString()));}
+        if (currentPizza != null) {
+            currentPizza.getToppings().stream().forEach(entry -> selectedToppings.add(entry.toString()));
+        }
 
         for (Topping topping : PIZZA_TOPPINGS) {
             CheckBox chkBox = new CheckBox(topping.toString());
-            if(selectedToppings != null){ chkBox.setSelected(selectedToppings.contains(topping.toString())); }
+            if (selectedToppings != null) {
+                chkBox.setSelected(selectedToppings.contains(topping.toString()));
+            }
             toppings.getChildren().add(chkBox);
         }
 
         Label lblExtras = new Label("Extra Instructions:");
         TextArea extras = new TextArea("");
         extras.setMinHeight(100);
-        if(currentPizza != null) { extras.setText(currentPizza.getExtras()); }
+        if (currentPizza != null) {
+            extras.setText(currentPizza.getExtras());
+        }
 
         Button btnSave = new Button("Save Pizza");
         Button btnCancel = new Button("Cancel Pizza");
 
         btnSave.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event){
-                // Save (re-assign currentPizza into a new variable so it can be edited in this function)
+            public void handle(ActionEvent event) {
+                // Save (re-assign currentPizza into a new variable so it can be edited in this
+                // function)
                 Pizza pizza = currentPizza;
-                
+
                 ArrayList<Topping> pizzaToppings = new ArrayList<Topping>();
                 ObservableList<Node> topping = toppings.getChildren();
-                
-                for(int i = 0; i < topping.size(); i++){
-                    CheckBox chk = (CheckBox)topping.get(i);
-                    if(chk.isSelected()){ pizzaToppings.add(PIZZA_TOPPINGS.get(i)); }
+
+                for (int i = 0; i < topping.size(); i++) {
+                    CheckBox chk = (CheckBox) topping.get(i);
+                    if (chk.isSelected()) {
+                        pizzaToppings.add(PIZZA_TOPPINGS.get(i));
+                    }
                 }
-                
-                if(pizza == null){ // Create new pizza
-                    int id = PIZZAS.size() > 0 ? PIZZAS.stream().collect(Collectors.maxBy(Comparator.comparing(Pizza::getId))).get().getId() + 1 : PIZZAS.size() + 1;
-                    pizza = new Pizza(id, PIZZA_SIZES.get(lvSizes.getSelectionModel().getSelectedIndex()), pizzaToppings, extras.getText());
+
+                if (pizza == null) { // Create new pizza
+                    int id = PIZZAS.size() > 0
+                            ? PIZZAS.stream().collect(Collectors.maxBy(Comparator.comparing(Pizza::getId))).get()
+                                    .getId() + 1
+                            : PIZZAS.size() + 1;
+                    pizza = new Pizza(id, PIZZA_SIZES.get(lvSizes.getSelectionModel().getSelectedIndex()),
+                            pizzaToppings, extras.getText());
                     PIZZAS.add(pizza);
-                }else { // Edit current pizza
+                } else { // Edit current pizza
                     pizza.setPizzaSize(PIZZA_SIZES.get(lvSizes.getSelectionModel().getSelectedIndex()));
                     pizza.setToppings(pizzaToppings);
                     pizza.setExtras(extras.getText());
                 }
-                
+
                 TABLE_VIEW.refresh(); // Refresh
 
                 // Always close after saving
                 primaryStage.close();
             }
         });
-        
+
         btnCancel.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event){
+            public void handle(ActionEvent event) {
                 primaryStage.close();
             }
         });
@@ -212,5 +239,23 @@ public class App extends Application {
         root.getChildren().addAll(lblSizes, lvSizes, lblToppings, toppings, lblExtras, extras, options);
         primaryStage.setScene(new Scene(root, 575, 350));
         primaryStage.show();
+    }
+
+    public static void ShowMessage(String message) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Message");
+        alert.setHeaderText("");
+        alert.setContentText(message);
+        alert.show();
+    }
+
+    public static boolean ShowPrompt(String message){
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Delete Pizza");
+        alert.setHeaderText(message);
+        alert.setContentText("Press OK to delete this item.");
+        Optional<ButtonType> result = alert.showAndWait();
+        System.out.println(result.get() == ButtonType.OK);
+        return result.isPresent() ? result.get() == ButtonType.OK : false;
     }
 }

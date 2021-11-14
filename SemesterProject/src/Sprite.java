dbb99f356663a311fa29fa4324daa85e8518d543
Ali Hussain <alihussain2001@gmail.com>
2021-11-14 14:43:22 -0500

    Some progress on sem project

--- SemesterProject/src/Sprite.java
+++ SemesterProject/src/Sprite.java
@@ -9,56 +9,5 @@ public class Sprite {
     private double width;
     private double height;
 
-    public Sprite(Image spritesheet, double x, double y, double xRect, double yRect, double width, double height) {
-        this.spritesheet = spritesheet;
-        this.x = x;
-        this.y = y;
-        this.xRect = xRect;
-        this.yRect = yRect;
-        this.width = width;
-        this.height = height;
-    }
-
-    public Image getSpritesheet() {
-        return spritesheet;
-    }
-    public void setSpritesheet(Image spritesheet) {
-        this.spritesheet = spritesheet;
-    }
-    public double getX() {
-        return x;
-    }
-    public void setX(double x) {
-        this.x = x;
-    }
-    public double getY() {
-        return y;
-    }
-    public void setY(double y) {
-        this.y = y;
-    }
-    public double getxRect() {
-        return xRect;
-    }
-    public void setxRect(double xRect) {
-        this.xRect = xRect;
-    }
-    public double getyRect() {
-        return yRect;
-    }
-    public void setyRect(double yRect) {
-        this.yRect = yRect;
-    }
-    public double getWidth() {
-        return width;
-    }
-    public void setWidth(double width) {
-        this.width = width;
-    }
-    public double getHeight() {
-        return height;
-    }
-    public void setHeight(double height) {
-        this.height = height;
-    }   
+    
 }

5a4ba34f798c3fc3b62fa3e448d3a3c906997f56
Ali Hussain <alihussain2001@gmail.com>
2021-12-08 14:06:08 -0500

    Update Player.java

--- SemesterProject/src/main/java/com/game/Entities/Player.java
+++ SemesterProject/src/main/java/com/game/Entities/Player.java
@@ -1,30 +1,35 @@
+// Physics copied from here --- https://gamedev.stackexchange.com/questions/70268/can-someone-explain-flappy-birds-physics-to-me
 package com.game.Entities;
-
-import com.game.App;
 import com.game.FPS;
-
 import javafx.scene.image.Image;
 
 public class Player extends Sprite {
     private double vSpeed = 0.0;
-    private double jSpeed = 300;
-    private double fallingSpeed = 50;
+    private double jSpeed = 35;
+    private double fallingSpeed = -100;
+    private boolean IS_DEAD = false;
 
     public Player(Image spritesheet){
         super(spritesheet);
     }
+    
+    public boolean getIsDead() { return IS_DEAD; }
+    public void setIsDead(boolean IS_DEAD) { this.IS_DEAD = IS_DEAD; }
 
     @Override
     public void onClick(){
         System.out.println("Player clicked!");
-        vSpeed = jSpeed;
+        if(!IS_DEAD) {
+        	vSpeed = jSpeed;
+        }
     }
 
     @Override
     public void update(){
         // Player physics go here
-        // System.out.println(FPS.getDeltaTime());
-        // this.getTransform().YPos += vSpeed * FPS.getDeltaTime();
-        // vSpeed += fallingSpeed * FPS.getDeltaTime();
+    	if(!IS_DEAD) {
+    		this.getTransform().YPos -= (vSpeed * FPS.getDeltaTime());
+            vSpeed += fallingSpeed * FPS.getDeltaTime();
+    	}
     }
 }
